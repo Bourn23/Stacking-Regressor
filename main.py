c9983f3d90eb3207aec3ec2153ce892bfc4610bd
Bourn23 <bourn23@gmail.com>
2018-12-19 14:59:53 +0330

    Successful

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-import pandas as pd
-"""
-Remember:
-0. how we picked from splitted data using iloc
-
-"""
-df_train1 = pd.read_csv('001_train.csv')
-predicators = ['Item_Weight', 'Item_Fat_Content',
-       'Item_MRP', 'Item_Visibility', 'Items_ID',
-        'Outlet_Size','Outlet_Age', 'Outlet_Location_Type', 'Outlet_Type' ]
-target = df_train1['Item_Outlet_Sales']
-df_train = df_train1.drop('Item_Outlet_Sales', axis = 1)
-df_train = df_train[predicators]
-
-df_test = pd.read_csv('001_test.csv')
-
-
-# print(df_train.head())
-# print('-------------TEST-----------')
-# print(df_test.head())
+from random import randint
 import pandas as pd
 import numpy as np
+
+#Loading Models
 from mlxtend.regressor import StackingRegressor
 from sklearn.model_selection import train_test_split, ShuffleSplit, KFold
 from sklearn.model_selection import cross_val_score, cross_validate, ShuffleSplit, GridSearchCV
@@ -30,53 +13,78 @@ from xgboost import XGBRegressor
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 
+
+# Loading Train & Test dataset
+df_train = pd.read_csv('001_train.csv')
+df_test = pd.read_csv('001_test.csv')
+
+
+# Predicators & Target columns
+predicators = ['Item_Weight', 'Item_Fat_Content',
+       'Item_MRP', 'Item_Visibility', 'Items_ID',
+        'Outlet_Size','Outlet_Age', 'Outlet_Location_Type', 'Outlet_Type' ]
+target = 'Item_Outlet_Sales'
+
+# Choosing base models
 base_models = [DecisionTreeRegressor, RandomForestRegressor, BaggingRegressor, AdaBoostRegressor, XGBRegressor]
 
-# stacking_train_dataset = np.matrix([[row for row in range(len(df_train))],[col for col in range(len(base_models))]])
+# Setting up train dataset for ensemble model
 row_train = len(df_train)
 col_train = len(base_models)
 stacking_train_dataset1 = np.zeros(shape = (row_train, col_train))
+stacking_train_dataset = pd.DataFrame(data= stacking_train_dataset1)
+
+
+# Setting up test dataset for ensemble model
 row_test = len(df_test)
 col_test = len(base_models)
 stacking_test_dataset1 = np.zeros(shape = (row_test, col_test))
-stacking_train_dataset = pd.DataFrame(data= stacking_train_dataset1)
 stacking_test_dataset = pd.DataFrame(data = stacking_test_dataset1)
-# def model_combiner (base_models, )
-split = KFold(n_splits=10, random_state=42)
-# print(len(df_train.columns))
+
+
+# Setting up KFold splitter
+number_of_split = 10
+split = KFold(n_splits = number_of_split, random_state = 42, shuffle = True)
+
+#Training Model and Filling up Train & Test dataset for stacking ensemble
 for i, base_alg in enumerate(base_models):
+    #using counter & inner_counter randomly chooses one of the split from dataset
+    #allowing each model being trained on different dataset
+    #different algorithm with different datasets
+    counter = randint(0, number_of_split)
+    inner_counter = 0
+
     for trainix , testix in split.split(stacking_train_dataset):
-        x_train = df_train.iloc[trainix]
-        y_train = target.iloc[trainix] #REMEMBER HOW WE CHOOSE ILOC
-        x_test = df_train.iloc[testix]
-        y_test = df_train.iloc[testix]
-        model = base_alg()
-        print(type(model))
-        # stacking_train_dataset['testcv', i] = 
-        # base_alg.fit(X, Y)#.predict(df_test[trainix])
-        model.fit(x_train, y_train)
-    #     base_alg.fit(df_train[rownum], target[row_num])
-    #     for row_num_testix in testix:
-        # print(type(stacking_train_dataset))
+        if inner_counter == counter:
+            x_train = df_train[predicators].iloc[trainix]
+            y_train = df_train[target].iloc[trainix] 
 
-        stacking_train_dataset['Item_Outlet_Sales', i] = model.predict(x_train)
-    for j in range(len(stacking_test_dataset)):
-        stacking_test_dataset [j, i] = model.fit(x_test, y_test).predict(df_test)
+            model = base_alg()
+            print('training : %s'%(type(model)))
+            model.fit(x_train, y_train)
 
+            #Filling up train & test datasets (CV) with the model predictions
+            stacking_train_dataset[i] = model.predict(df_train[predicators])
+            stacking_test_dataset[i] = model.predict(df_test[predicators])
+
+        inner_counter += 1
+
+
+# You can change the ensemble model here
 ensemble_model = LinearRegression()
 
-# for i in range(len(stacking_test_dataset)):
 
+# Ensemble Model Prediction
+stacking_train_dataset['Ensemble'] = ensemble_model.fit(stacking_train_dataset, df_train[target]).predict(stacking_train_dataset)
 
-df_predict_final = ensemble_model.fit(stacking_train_dataset, target).predict(stacking_test_dataset)
-print(type(df_predict_final))
+# Preparing for sumbission
+IDcol = ['Item_Identifier', 'Outlet_Identifier']
+submission = df_test[IDcol]
+submission[target] = ensemble_model.predict(stacking_test_dataset)
+submission.to_csv('Submit01.csv', index = False)
 
-df_predict = pd.DataFrame(data = pd_predict_final)
-df_stack_test.to_csv('stacking_test.csv', index = False)
-df_stack_train.to_csv('stacking_train.csv', index = False)
-df_predict_final.to_csv('stacking_predict.csv', index = False)
+stacking_test_dataset['Ensemble_Output'] = ensemble_model.predict(stacking_test_dataset)
 
-
-
-
-
+# Saving Created Datasets
+stacking_test_dataset.to_csv('stacking_test.csv', index = False)
+stacking_train_dataset.to_csv('stacking_train.csv', index = False)
